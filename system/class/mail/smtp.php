3575f39ade328d7e1b107c35ccfb4952a513135d
unknown <wb.pengwei@game.ntes>
2018-01-22 18:26:04 +0800

    优化 SMTP 功能，大幅提升速度，支持 SSL 465 安全特性

--- system/class/mail/smtp.php
+++ system/class/mail/smtp.php
@@ -1,146 +1,240 @@
 <?php
-if(!defined('IN_KKFRAME')) exit('Access Denied');
+if (!defined('IN_KKFRAME')) exit('Access Denied');
 
-class smtp extends mailer{
-	var $id = 'smtp';
-	var $name = 'SMTP - Socket';
-	var $description = '通过 Socket 连接 SMTP 服务器发邮件';
-	var $config = array(
-		array('SMTP 服务器地址', 'smtp_server', '', ''),
-		array('SMTP 服务器端口', 'smtp_port', '', '25', 'number'),
-		array('SMTP 用户名', 'smtp_name', '', ''),
-		array('SMTP 密码', 'smtp_pass', '', '', 'password'),
-		array('发送者邮箱地址', 'address', '一般与 用户名 一致', '', 'email')
+class smtp extends mailer {
+    var $id = 'smtp';
+    var $name = 'SMTP - Socket';
+    var $description = '通过 Socket 连接 SMTP 服务器发邮件 - Vrsion: v2.0';
+    var $config = array(
+        array('SMTP 服务器地址', 'server', '', '') ,
+        array('SMTP 服务器端口', 'port', '', '465', 'number') ,
+        array('SMTP 用户名', 'name', '', '') ,
+		array('SMTP 密码', 'pass', '', '', 'password') ,
+		array('发送者名称', 'fromname', '', '贴吧签到助手'),
+		array('发送者邮箱', 'address', '一般与用户名一致', '', 'email')
 	);
-	function isAvailable(){
-		return !isset($_SERVER['HTTP_APPVERSION']) && $_SERVER['USER'] != 'bae';
+
+    function isAvailable() {
+        return !isset($_SERVER['HTTP_APPVERSION']) && $_SERVER['USER'] != 'bae';
 	}
-	function send($mail){
-		$smtp = new _smtp($this);
-		return $smtp->send($mail->address, $mail->subject, $mail->message);
-	}
+
+    function send($mail) {
+        $smtp = new _smtp($this);
+        $smtp->to($mail->address);
+        $smtp->subject($mail->subject);
+        $smtp->message($mail->message);
+        return $smtp->send();
+    }
 }
 
 class _smtp {
-	private $smtpServer = '';
-	private $port = '';
-	private $timeout = '45';
-	private $username = '';
-	private $password = '';
-	private $address = '';
-	private $newline = "\r\n";
-	private $localdomain = 'localhost';
-	private $charset = 'utf-8';
-	private $contentTransferEncoding = false;
-	private $debug = false;
+    // connection
+    protected $connection;
+    protected $localhost = 'localhost';
+    protected $timeout = 30;
+    protected $debug_mode = FALSE;
+    // auth
+    protected $smtpServer;
+    protected $smtpPort;
+    protected $secure; // null, 'ssl', or 'tls'
+    protected $auth; // 如果需要验证，改为 True
+    protected $smtpName;
+    protected $smtpPass;
+    // email
+    protected $to = []; // 收信人
+    protected $cc = []; // 抄送人
+    protected $bcc = []; // 密送人
+    protected $fromname; // 发送人名称
+    protected $address; // 发送人地址
+    protected $from; // 发送人
+    protected $message; // 内容
+    protected $subject; // 标题
+    // misc
+    protected $charset = 'UTF-8';
+    protected $newline = "\r\n";
+	protected $encoding = '7bit';
 
-	private $smtpConnect = false;
-	private $to = false;
-	private $subject = false;
-	private $message = false;
-	private $headers = false;
-	private $logArray = array();
-	public $Error = '';
-
-	public function _smtp($obj){
-		$this->smtpServer = $obj->_get_setting('smtp_server');
-		$this->port = $obj->_get_setting('smtp_port');
+    public function __construct($obj) {
+        $this->smtpServer = $obj->_get_setting('server');
+        $this->smtpPort = $obj->_get_setting('port');
+        $this->secure = ($this->smtpPort == 465) ? 'ssl' : '';
+        $this->auth = TRUE;
+        $this->smtpName = $obj->_get_setting('name');
+        $this->smtpPass = $obj->_get_setting('pass');
+        $this->fromname = $obj->_get_setting('fromname');
 		$this->address = $obj->_get_setting('address');
-		$this->username = $obj->_get_setting('smtp_name');
-		$this->password = $obj->_get_setting('smtp_pass');
-		if ($this->port == '465') $this->smtpServer = 'ssl://' . $this->smtpServer;
+		$this->from();
 	}
 
-	public function send($to, $subject, $message) {
-		global $_config;
-		$this->to = &$to;
-		$this->subject = &$subject;
-		$this->message = &$message;
+    public function from() {
+        $this->from = array(
+            'email' => $this->address,
+            'name' => $this->fromname
+        );
+    }
 
-		if(!$this->Connect2Server()) {
-			if(!$this->debug) return false;
-			echo $this->Error.$this->newline.'<!-- '.$this->newline;
-			print_r($this->logArray);
-			echo $this->newline.'-->'.$this->newline;
-			return false;
-		}
-		return true;
+    public function to($email, $name = null) {
+        $this->to[] = array(
+            'email' => $email,
+            'name' => $name
+        );
 	}
 
-	private function Connect2Server() {
-		$this->smtpConnect = fsockopen($this->smtpServer, $this->port, $errno, $error, $this->timeout);
-		$this->logArray['CONNECT_RESPONSE'] = $this->readResponse();
-		if (!is_resource($this->smtpConnect)) return false;
-		$this->logArray['connection'] = 'Connection accepted: '.$this->readResponse();
-		$this->sendCommand("EHLO {$this->localdomain}");
-		$this->logArray['EHLO'] = $this->readResponse();
-		$this->sendCommand('AUTH LOGIN');
-		$this->logArray['AUTH_REQUEST'] = $this->readResponse();
-		$this->sendCommand(base64_encode($this->username));
-		$this->logArray['REQUEST_USER'] = $this->readResponse();
-		$this->sendCommand(base64_encode($this->password));
-		$this->logArray['REQUEST_PASSWD'] = $this->readResponse();
-		if (substr($this->logArray['REQUEST_PASSWD'], 0, 3)!='235') {
-			$this->Error .= 'Authorization error! '.$this->logArray['REQUEST_PASSWD'].$this->newline;
-			return false;
-		}
-		$this->sendCommand("MAIL FROM: {$this->address}");
-		$this->logArray['MAIL_FROM_RESPONSE'] = $this->readResponse();
-		if (substr($this->logArray['MAIL_FROM_RESPONSE'], 0, 3)!='250') {
-			$this->Error .= 'Mistake in sender\'s address! '.$this->logArray['MAIL_FROM_RESPONSE'].$this->newline;
-			return false;
-		}
-		$this->sendCommand("RCPT TO: {$this->to}");
-		$this->logArray['RCPT_TO_RESPONCE'] = $this->readResponse();
-		if (substr($this->logArray['RCPT_TO_RESPONCE'], 0, 3)!='250') {
-			$this->Error .= 'Mistake in reciepent address! '.$this->logArray['RCPT_TO_RESPONCE'].$this->newline;
-		}
-		$this->sendCommand('DATA');
-		$this->logArray['DATA_RESPONSE'] = $this->readResponse();
-		if (!$this->sendMail()) return false;
-		$this->sendCommand('QUIT');
-		$this->logArray['QUIT_RESPONSE'] = $this->readResponse();
-		fclose($this->smtpConnect);
-		return true;
+    public function cc($email, $name = null) {
+        $this->cc[] = array(
+            'email' => $email,
+            'name' => $name
+        );
 	}
-	private function sendMail() {
-		$this->sendHeaders();
-		$this->sendCommand($this->message);
-		$this->sendCommand('.');
-		$this->logArray['SEND_DATA_RESPONSE'] = $this->readResponse();
-		if(substr($this->logArray['SEND_DATA_RESPONSE'], 0, 3)!='250') {
-			$this->Error .= 'Mistake in sending data! '.$this->logArray['SEND_DATA_RESPONSE'].$this->newline;
-			return false;
-		}
-		return true;
+
+    public function bcc($email, $name = null) {
+        $this->bcc[] = array(
+            'email' => $email,
+            'name' => $name
+        );
 	}
-	private function readResponse() {
-		$data = '';
-		while($str = fgets($this->smtpConnect, 4096)) {
-			$data .= $str;
-			if(substr($str, 3, 1) == " ") { break; }
-		}
-		return $data;
+
+    public function subject($subject) {
+        $this->subject = $subject;
 	}
-	private function sendCommand($string) {
-		fputs($this->smtpConnect, $string.$this->newline);
-		return ;
+
+    public function message($html) {
+        $this->message = $html;
 	}
-	private function sendHeaders() {
-		$this->sendCommand('Date: '.date('D, j M Y G:i:s').' +0700');
-		$this->sendCommand("From: <{$this->address}>");
-		$this->sendCommand("Reply-To: <{$this->address}>");
-		$this->sendCommand("To: <{$this->to}>");
-		$this->sendCommand("Subject: {$this->subject}");
-		$this->sendCommand('MIME-Version: 1.0');
-		$this->sendCommand("Content-Type: text/html; charset={$this->charset}");
-		if ($this->contentTransferEncoding) $this->sendCommand("Content-Transfer-Encoding: {$this->contentTransferEncoding}");
-		$this->sendCommand($this->newline);
-		return ;
+
+    public function send() {
+        if ($this->smtp_connect()) {
+            if ($this->smtp_deliver()) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        } else {
+            $result = false;
+        }
+        $this->smtp_disconnect();
+        return $result;
 	}
-	public function __destruct() {
-		if (is_resource($this->smtpConnect)) fclose($this->smtpConnect);
+
+    protected function smtp_connect() {
+        if ($this->secure === 'ssl') $this->smtpServer = 'ssl://' . $this->smtpServer;
+        $this->connection = fsockopen($this->smtpServer, $this->smtpPort, $errno, $errstr, $this->timeout);
+        if ($this->code() !== 220) return false;
+        $this->request(($this->auth ? 'EHLO' : 'HELO') . ' ' . $this->localhost . $this->newline);
+        $this->response();
+        if ($this->secure === 'tls') {
+            $this->request('STARTTLS' . $this->newline);
+            if ($this->code() !== 220) return false;
+            stream_socket_enable_crypto($this->connection, true, STREAM_CRYPTO_METHOD_TLS_CLIENT);
+            $this->request(($this->auth ? 'EHLO' : 'HELO') . ' ' . $this->localhost . $this->newline);
+            if ($this->code() !== 250) return false;
+        }
+        if ($this->auth) {
+            $this->request('AUTH LOGIN' . $this->newline);
+            if ($this->code() !== 334) return false;
+            $this->request(base64_encode($this->smtpName) . $this->newline);
+            if ($this->code() !== 334) return false;
+            $this->request(base64_encode($this->smtpPass) . $this->newline);
+            if ($this->code() !== 235) return false;
+        }
+        return true;
 	}
+
+    protected function smtp_deliver() {
+        $this->request('MAIL FROM: <' . $this->from['email'] . '>' . $this->newline);
+        $this->response();
+        $recipients = array_merge($this->to, $this->cc, $this->bcc);
+        foreach ($recipients as $r) {
+            $this->request('RCPT TO: <' . $r['email'] . '>' . $this->newline);
+            $this->response();
+        }
+        $this->request('DATA' . $this->newline);
+        $this->response();
+        $this->request($this->smtp_construct());
+        if ($this->code() === 250) {
+            return true;
+        } else {
+            return false;
+        }
+	}
+
+    protected function smtp_construct() {
+        $boundary = md5(uniqid(time()));
+        $headers[] = 'From: ' . $this->format($this->from);
+        $headers[] = 'Subject: ' . $this->subject;
+        $headers[] = 'Date: ' . date('r');
+
+        if (!empty($this->to)) {
+            $string = '';
+            foreach ($this->to as $r) $string.= $this->format($r) . ', ';
+            $string = substr($string, 0, -2);
+            $headers[] = 'To: ' . $string;
+        }
+
+        if (!empty($this->cc)) {
+            $string = '';
+            foreach ($this->cc as $r) $string.= $this->format($r) . ', ';
+            $string = substr($string, 0, -2);
+            $headers[] = 'CC: ' . $string;
+        }
+
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-Type: multipart/mixed; boundary="' . $boundary . '"';
+        $headers[] = '';
+        $headers[] = 'This is a multi-part message in MIME format.';
+        $headers[] = '--' . $boundary;
+
+        $headers[] = 'Content-Type: text/html; charset="' . $this->charset . '"';
+        $headers[] = 'Content-Transfer-Encoding: ' . $this->encoding;
+        $headers[] = '';
+        $headers[] = $this->message;
+        $headers[] = '--' . $boundary;
+
+        $headers[sizeof($headers) - 1].= '--';
+        $headers[] = '.';
+
+        $email = '';
+        foreach ($headers as $header) {
+            $email.= $header . $this->newline;
+        }
+        return $email;
+	}
+
+    protected function smtp_disconnect() {
+        $this->request('QUIT' . $this->newline);
+        $this->response();
+        fclose($this->connection);
+	}
+
+    protected function code() {
+        return (int)substr($this->response(), 0, 3);
+	}
+
+    protected function request($string) {
+        if ($this->debug_mode) echo '<code><strong>' . $string . '</strong></code><br/>';
+        fputs($this->connection, $string);
+	}
+
+    protected function response() {
+        $response = '';
+        while ($str = fgets($this->connection, 4096)) {
+            $response.= $str;
+            if (substr($str, 3, 1) === ' ') break;
+        }
+        if ($this->debug_mode) echo '<code>' . $response . '</code><br/>';
+        return $response;
+	}
+
+    protected function format($recipient) {
+        // 格式 "name <email>"
+        if ($recipient['name']) {
+            return $recipient['name'] . ' <' . $recipient['email'] . '>';
+        } else {
+            return '<' . $recipient['email'] . '>';
+        }
+	}
+
+    public function __destruct() {
+        if (is_resource($this->connection)) smtp_disconnect();
+    }
 }
-
-?>
